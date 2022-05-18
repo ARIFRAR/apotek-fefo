5937d8187192b00c16bff3a001030d69a26be3db
Hadi Firmansyah <hadifirmansyah48@gmail.com>
2022-05-18 12:04:17 +0800

    fix sidebar

--- resources/views/inc/sidebar.blade.php
+++ resources/views/inc/sidebar.blade.php
@@ -1,5 +1,3 @@
-@if ($page_name != 'coming_soon' && $page_name != 'contact_us' && $page_name != 'error404' && $page_name != 'error500' && $page_name != 'error503' && $page_name != 'faq' && $page_name != 'helpdesk' && $page_name != 'maintenence' && $page_name != 'privacy' && $page_name != 'auth_boxed' && $page_name != 'auth_default')
-
     <!--  BEGIN SIDEBAR  -->
     <div class="sidebar-wrapper sidebar-theme">
 
@@ -158,4 +156,3 @@
     </div>
     <!--  END SIDEBAR  -->
 
-@endif
